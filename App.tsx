50fea3e5aa08afa97736e97010d78e4a6cdb4372
Edmilson Soares <esbnet@gmail.com>
2022-07-19 02:43:57 -0300

    feat: :sparkles: add filters button - home

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,15 @@
-import { VStack, HStack, IconButton, useTheme } from 'native-base'
+import { useState } from 'react';
+
+import { VStack, HStack, IconButton, useTheme, Text, Heading } from 'native-base'
 import { SignOut } from 'phosphor-react-native'
 
 import Logo from '../assets/logo_secondary.svg'
+import { Filter } from '../components/Filter';
 
 export function Home() {
+
+    const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('closed');
+
     const { colors } = useTheme();
     return (
         <VStack flex={1} pb={6} bg="gray.700" alignItems={"center"}>
@@ -21,6 +27,29 @@ export function Home() {
                     icon={<SignOut size={26} color={colors.gray[300]} />}
                 />
             </HStack>
+
+            <VStack flex={1} px={6}>
+                <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
+                    <Heading color="gray.100">
+                        Meus chamados
+                    </Heading>
+                    <Text color="gray.200">3</Text>
+                </HStack>
+                <HStack space={3} mb={8}>
+                    <Filter
+                        type='open'
+                        title='em andamento'
+                        onPress={() => setStatusSelected('open')}
+                        isActive={statusSelected === 'open'}
+                    />
+                    <Filter
+                        type='closed'
+                        title='finalizado'
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'}
+                    />
+                </HStack>
+            </VStack>
         </VStack>
     )
 }
